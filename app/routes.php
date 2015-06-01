dae8493bab801f58987d0a895556240546795b7b
Arian Castillo <caztle501@gmail.com>
2015-05-31 21:11:22 -0500

    Correcciones para cerrar sesión

--- app/routes.php
+++ app/routes.php
@@ -13,13 +13,6 @@
 
 Route::group(array('before' => 'auth'), function()
 {
-	//Cerrar sesión
-	Route::get('logout', 'SessionsController@logOut');
-	Route::get('/supplier/logout', 'SessionsController@logOut');
-	Route::get('/income/logout', 'SessionsController@logOut');
-
-
-
 	//Crear vista alta proveedores
 	Route::get('/supplier/create', function()
 	{
@@ -129,3 +122,8 @@ Route::get('/', 'SessionsController@showLogin');
 
 //Verificar los datos ingresados en el formulario y enviar al menu principal
 Route::post('welcome', 'SessionsController@welcome');
+
+//Cerrar sesión
+Route::get('logout', 'SessionsController@logOut');
+Route::get('/supplier/logout', 'SessionsController@logOut');
+Route::get('/income/logout', 'SessionsController@logOut');
