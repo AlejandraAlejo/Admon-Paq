80b57fa5152b5b40d9ff9d3489f1ae14a0847bd8
Arian Castillo <caztle501@gmail.com>
2015-05-31 23:39:40 -0500

    Eliminación en cascada: si se elimina el proveedor se eliminan los egresos

--- app/models/Supplier.php
+++ app/models/Supplier.php
@@ -1,9 +1,21 @@
 <?php
 
 class Supplier extends Eloquent{
-	public function expense()
+	public function expenses()
 	{
 		return $this->hasMany('Expense');
 	}
+
+	public function delete()
+    {
+        if(count($this->expenses) > 0){
+	        foreach($this->expenses as $expense)
+	        {
+	            $expense->delete();
+	        }
+	    }
+ 
+        return parent::delete();
+    }
 }
 ?>
