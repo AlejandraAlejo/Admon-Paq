a2808148b50d0bbeb3471d53a49ff07a4f6e6772
Alejandra Alejo <ale_alejor@hotmail.com>
2015-06-01 11:59:02 -0500

    cambios visuales a avisos de error

--- app/views/incomes/save.blade.php
+++ app/views/incomes/save.blade.php
@@ -22,19 +22,19 @@ Registrar Ingreso
     <div class = "form-group">
         {{ Form::label('description', 'Concepto') }}
         {{ Form::text('description', Input::old('description'),array('class' => 'form-control', 'placeholder' => 'Concepto')) }}
-        <div class="bg-danger">{{$errors->first('description')}}</div>
+        {{ $errors->first('description', '<span class="label label-danger">:message</span>') }}
     </div>
                 
     <div class = "form-group">
         {{ Form::label('date', 'Fecha del Ingreso (Año-Mes-Día)') }}
         {{ Form::text('date', Input::old('date'), array('id' => 'datepicker', 'class' => 'form-control', 'placeholder' => 'Fecha')) }}
-        <div class="bg-danger">{{$errors->first('date')}}</div>
+        {{ $errors->first('date', '<span class="label label-danger">:message</span>') }}
     </div>
 
     <div class = "form-group">
         {{ Form::label('amount', 'Cantidad') }}
         {{ Form::text('amount', Input::old('amount'), array('class' => 'form-control', 'placeholder' => 'Cantidad')) }}
-        <div class="bg-danger">{{$errors->first('amount')}}</div>
+        {{ $errors->first('amount', '<span class="label label-danger">:message</span>') }}
     </div>
     
     <div class = "submit-button">
--- app/views/incomes/update.blade.php
+++ app/views/incomes/update.blade.php
@@ -21,19 +21,19 @@ Registrar Ingreso
     <div class = "form-group">
         {{ Form::label('description', 'Concepto') }}
         {{ Form::text('description', $income->description,array('class' => 'form-control', 'placeholder' => 'Concepto')) }}
-        <div class="bg-danger">{{$errors->first('description')}}</div>
+        {{ $errors->first('description', '<span class="label label-danger">:message</span>') }}
     </div>
                 
     <div class = "form-group">
         {{ Form::label('date', 'Fecha del Ingreso (Año-Mes-Día)') }}
         {{ Form::text('date', $income->date, array('id' => 'datepicker', 'class' => 'form-control', 'placeholder' => 'Fecha')) }}
-        <div class="bg-danger">{{$errors->first('date')}}</div>
+        {{ $errors->first('date', '<span class="label label-danger">:message</span>') }}
     </div>
 
     <div class = "form-group">
         {{ Form::label('amount', 'Cantidad') }}
         {{ Form::text('amount', $income->amount, array('class' => 'form-control', 'placeholder' => 'Cantidad')) }}
-        <div class="bg-danger">{{$errors->first('amount')}}</div>
+        {{ $errors->first('amount', '<span class="label label-danger">:message</span>') }}
     </div>
     
     <div class = "submit-button">
